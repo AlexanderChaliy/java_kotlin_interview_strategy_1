ac7f8dfac89ef568ac3be668cd3dadd89a441421
Oleksandr Chalyi <olexandr.chalyi@atola.com>
2024-10-28 21:22:44 +0200

    Task3 - Added CustomStack.java

---
+++ src/main/java/CustomStack.java
@@ -0,0 +1,13 @@
+public class CustomStack {
+    public void push(int x) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int pop() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getMax(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
